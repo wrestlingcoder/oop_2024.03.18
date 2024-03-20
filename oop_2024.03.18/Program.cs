223f5fe590fba68ece1f51197d4323d72dbfb583
wrestlingcoder <a.m.myagtin@gmail.com>
2024-03-20 18:28:13 +0300

    ver1

--- oop_2024.03.18/Program.cs
+++ oop_2024.03.18/Program.cs
@@ -4,16 +4,23 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace oop_2024._03._18
 {
-     class Program
+    class Program
     {
         static void Main(string[] args)
         {
             List<Person> users = new List<Person>(); // создаем список users из элементов класса Person
             while (true)
             {
+                int i = 5;
+                string s = i.ToString();
+                int j = 999;
+                int v = j.GetHashCode();
+
+                int p = int.Parse("99");
 
                 Console.WriteLine("Введите данные нового пользователя, или Выход для выхода:");
                 string userString = Console.ReadLine();
@@ -29,19 +36,24 @@ namespace oop_2024._03._18
 
                 if (userArray.Length == 4)
                 {
-                   int userAge = Convert.ToInt32(userArray[2]);
-                   newUser = new Person(userArray[0], userArray[1], userAge, userArray[3]);
+                    int userAge = Convert.ToInt32(userArray[2]);
+                    newUser = new Person(userArray[0], userArray[1], userAge, userArray[3]);
                 }
                 else
                 {
                     int userAge = Convert.ToInt32(userArray[1]);
-                    newUser = new Person(userArray[0], userAge, userArray[2]);
+                    string nameAndSurname = userArray[0];
+                    newUser = new Person(nameAndSurname, userAge, userArray[2]); //конструктор
+                   
+                        
                 }
+
+                
                 users.Add(newUser); // добавляем нового пользователя в список элементов класса Person
             }
 
             Console.WriteLine("Список добавленных пользователей:");
-            foreach(Person user in users)
+            foreach (Person user in users)
             {
                 Console.WriteLine(user.GetInfo(true));
             }
@@ -51,9 +63,26 @@ namespace oop_2024._03._18
 
     class Person
     {
+        public bool IsValid = false;  // переменная некорректного элемента, если класс создан некорректно IsValid = false
         public string Name = "Unknown";
         public string Surname;
-        public int Age;
+        int _age; //private int _age; внутренняя переменная внутри класса Person
+
+        
+        public int Age  //свойство класса
+        {
+            get //возможность получить значение
+            {
+
+                if (_age < 0) return 0;
+                return _age;
+            }
+            set //возможность з значение
+            {
+                if (value > 0) _age = value; // value - виртуальная конструкция
+                else _age = 0;
+            }
+        }
         public string Company = "Пик-Проект";
 
         /// <summary>
@@ -66,19 +95,29 @@ namespace oop_2024._03._18
         public Person(string name, string surname, int age, string company) //вводим конструктор Person 
 
         {
-            Name = name.Replace(" ","");
+            Name = name.Replace(" ", "");
             Surname = surname.Replace(" ", "");
-            Age = age;
+            if (age < 0)
+            {
+                _age = 0;
+            }
+            else
+            {
+                _age = age;
+            }
+
             Company = company.Replace(" ", "");
         }
 
-
+        /// <summary>
+        /// /1231234
+        /// </summary>
         public void ShowInfo()  //вводим Метод ShowInfo 
 
         {
             Console.WriteLine("Новый пользователь: " + Surname + " " + Name + ", " + Age.ToString());
         }
-        
+
 
         /// <summary>
         /// Указать информацию, когда имя и фамилия в одной строке
@@ -87,17 +126,28 @@ namespace oop_2024._03._18
         /// <param name="surname"></param>
         /// <param name="age"></param>
         /// <param name="company"></param>
-        public Person(string nameAndSurname, int age, string company)
+        public Person(string nameAndSurname, int age, string company) // вводим конструктор Person
         {
-            Name = nameAndSurname.Split(' ')[0];
-            Surname = nameAndSurname.Split(' ')[1];
-            Age = age;
+            string[] array = nameAndSurname.Split(' ');
+            if (array.Length == 2)
+            {
+                Name = array[0];
+                Surname = array[1];
+                IsValid = true;
+            }
+            else 
+            {
+                Name = array[0];
+                Surname = "Бесфамильный";
+                IsValid = false;
+            }
+            _age = age;
             Company = company;
         }
 
         public string GetInfo(bool GetExtendedInfo) // вводим метод и указываем тип возвращаемого значения и нужно вернуть значение
-        { 
-            if (GetExtendedInfo) 
+        {
+            if (GetExtendedInfo)
             {
                 string info = Surname + " " + Name + ", " + Age.ToString() + ", " + Company;
                 return info;
@@ -107,7 +157,7 @@ namespace oop_2024._03._18
                 string info = Surname + " " + Name + ", " + Age.ToString();
                 return info;
             }
-        
+
         }
     }
 }
